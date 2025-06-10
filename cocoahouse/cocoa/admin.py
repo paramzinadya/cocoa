fc9025a9659665b9c69788708892f8600df7266e
Nadya Paramzina <parramzina@mail.ru>
2025-06-11 01:28:42 +0700

    pravki

--- cocoahouse/cocoa/admin.py
+++ cocoahouse/cocoa/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post, Category
+from datetime import date
 
 class FilialFilter(admin.SimpleListFilter):
  title = 'Наличие филиала'
@@ -16,10 +17,10 @@ class FilialFilter(admin.SimpleListFilter):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    actions = ['set_published']
+    actions = ['set_draft', 'set_published']
     fields = ('title', 'slug', 'content', 'cat', 'filial', 'is_published')
     prepopulated_fields = {"slug": ("title",)}
-    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info')
+    list_display = ('title', 'time_create', 'is_published', 'cat', 'brief_info', 'days_since_publication')
     list_display_links = ('title', )
     list_editable = ('is_published', )
     ordering = ['-time_create', 'title']
@@ -30,15 +31,25 @@ class PostAdmin(admin.ModelAdmin):
     def brief_info(self, post: Post):
         return f"Описание {len(post.content)} символов."
 
+    @admin.display(description="Дней с публикации")
+    def days_since_publication(self, post: Post):
+        if post.time_create:
+            delta = date.today() - post.time_create.date()
+            return f"{delta.days} дн."
+        return "Нет даты"
+
+    @admin.action(description="Снять с публикации выбранные записи")
+    def set_draft(self, request, queryset):
+        count = queryset.update(is_published=Post.Status.DRAFT)
+        self.message_user(request, f"Снято с публикации {count} записи(ей)")
+
     @admin.action(description="Опубликовать выбранные записи")
     def set_published(self, request, queryset):
         count = queryset.update(is_published=Post.Status.PUBLISHED)
         self.message_user(request, f"Изменено {count} записи(ей)")
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
  list_display = ('id', 'name')
  list_display_links = ('id', 'name')
-
-
-
--- cocoahouse/cocoa/forms.py
+++ cocoahouse/cocoa/forms.py
@@ -1,11 +1,29 @@
 from django import forms
+from django.utils.deconstruct import deconstructible
+from prompt_toolkit.validation import ValidationError
+
 from .models import Category, Filial
+from django.core.validators import MinLengthValidator,MaxLengthValidator
+
+@deconstructible
+class RussianValidator:
+ ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
+ code = 'russian'
+ def __init__(self, message=None):
+  self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
+ def __call__(self, value):
+  if not (set(value) <= set(self.ALLOWED_CHARS)):
+   raise ValidationError(self.message, code=self.code, params={"value": value})
 
 class AddPostForm(forms.Form):
- title = forms.CharField(max_length=255, label="Заголовок")
- slug = forms.SlugField(max_length=255, label="URL")
- content = forms.CharField(widget=forms.Textarea(), required=False, label="Содержание поста")
- is_published = forms.BooleanField(required=False, label="Статус")
+ title = forms.CharField(max_length=255,
+                         min_length=5, label="Заголовок",
+                         widget=forms.TextInput(attrs={'class': 'form - input'}),
+                         validators = [RussianValidator(),],
+                         error_messages = {'min_length': 'Слишком короткий заголовок','required': 'Без заголовка - никак',})
+ slug = forms.SlugField(max_length=255, label="URL", validators=[MinLengthValidator(5, message="Минимум 5 символов"), MaxLengthValidator(100, message="Максимум 100 символов"),])
+ content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Содержание поста")
+ is_published = forms.BooleanField(required=False, label="Опубликовать")
  cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории")
  filial = forms.ModelChoiceField(queryset=Filial.objects.all(), required=False, label="Филиал")
 
