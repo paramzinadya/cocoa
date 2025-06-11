4fc0f2c1a8535b455bf189931efc80481de4dcef
Nadya Paramzina <parramzina@mail.ru>
2025-06-11 17:49:55 +0700

    загрузка файлов и правки

--- cocoahouse/cocoa/forms.py
+++ cocoahouse/cocoa/forms.py
@@ -1,29 +1,34 @@
 from django import forms
 from django.utils.deconstruct import deconstructible
-from prompt_toolkit.validation import ValidationError
+from django.core.exceptions import ValidationError
 
-from .models import Category, Filial
+from .models import Category, Filial, Post
 from django.core.validators import MinLengthValidator,MaxLengthValidator
 
-@deconstructible
-class RussianValidator:
+def clean_title(self):
+ title = self.cleaned_data['title']
  ALLOWED_CHARS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯабвгдеёжзийклмнопрстуфхцчшщбыъэюя0123456789- "
- code = 'russian'
- def __init__(self, message=None):
-  self.message = message if message else "Должны присутствовать только русские символы, дефис и пробел."
- def __call__(self, value):
-  if not (set(value) <= set(self.ALLOWED_CHARS)):
-   raise ValidationError(self.message, code=self.code, params={"value": value})
+ if not (set(title) <= set(ALLOWED_CHARS)):
+     raise ValidationError("Должны быть только русские символы, дефис и пробел.")
+ return title
 
-class AddPostForm(forms.Form):
- title = forms.CharField(max_length=255,
-                         min_length=5, label="Заголовок",
-                         widget=forms.TextInput(attrs={'class': 'form - input'}),
-                         validators = [RussianValidator(),],
-                         error_messages = {'min_length': 'Слишком короткий заголовок','required': 'Без заголовка - никак',})
- slug = forms.SlugField(max_length=255, label="URL", validators=[MinLengthValidator(5, message="Минимум 5 символов"), MaxLengthValidator(100, message="Максимум 100 символов"),])
- content = forms.CharField(widget=forms.Textarea(attrs={'cols': 50, 'rows': 5}), required=False, label="Содержание поста")
- is_published = forms.BooleanField(required=False, label="Опубликовать")
- cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категории")
- filial = forms.ModelChoiceField(queryset=Filial.objects.all(), required=False, label="Филиал")
 
+class AddPostForm(forms.ModelForm):
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 50:
+            raise ValidationError('Длина превышает 50 символов')
+        return title
+
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Категория не выбрана", label="Категории")
+    filial = forms.ModelChoiceField(queryset=Filial.objects.all(),required=False, empty_label="Нет филиала", label="Филиал")
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'content', 'is_published', 'cat', 'filial', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
+class UploadFileForm(forms.Form):
+ file = forms.FileField(label="Файл")
--- cocoahouse/cocoa/views.py
+++ cocoahouse/cocoa/views.py
@@ -1,13 +1,13 @@
 from calendar import month
 from datetime import datetime
 from django.template.loader import render_to_string
-
-
+from django.core.exceptions import ValidationError
+import uuid
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-from cocoa.forms import AddPostForm
+from cocoa.forms import AddPostForm, UploadFileForm
 from cocoa.models import Post, Category, TagPost, Filial
 
 main_menu = [{'title': "О сайте", 'url_name': 'about'},
@@ -28,9 +28,25 @@ def index(request):
  }
  return render(request, 'cocoa/index.html', context=data)
 
+def handle_uploaded_file(f):
+    name = f.name
+    ext = ''
+    if '.' in name:
+        ext = name[name.rindex('.'):]
+        name = name[:name.rindex('.')]
+        suffix = str(uuid.uuid4())
+    with open(f"uploads/{name}_{suffix}{ext}", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 
 def about(request):
- return render(request, 'cocoa/about.html',{'title': 'О сайте', 'main_menu': main_menu})
+    if request.method == "POST":
+        form = UploadFileForm(request.POST,request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(form.cleaned_data['file'])
+    else:
+        form = UploadFileForm()
+    return render(request, 'cocoa/about.html',{'title': 'О сайте', 'menu': menu, 'form': form})
 
 def catalog(request):
  return render(request, 'cocoa/catalog.html',{'title': 'Каталог', 'main_menu': main_menu})
@@ -141,11 +157,8 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            try:
-                Post.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
         # НЕ создаём заново form в этом else!
     else:
         form = AddPostForm()  # ← создаём форму при GET
